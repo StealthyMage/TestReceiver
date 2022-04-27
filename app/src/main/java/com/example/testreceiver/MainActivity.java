726cc26fdb65647d543f0937754ed47425006d2d
StealthyMage <36330911+StealthyMage@users.noreply.github.com>
2022-04-27 13:37:41 +1000

    Creation of Test Receiver for Android App Shenanigans
    We do the things n stuff

---
+++ app/src/main/java/com/example/testreceiver/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.testreceiver;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    TextView movies;
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.recycler_layout);
+        //recyclerView = findViewById(R.id.recycler_layout_id);
+        //layoutManager = new LinearLayoutManager(this);
+        //recyclerView.setLayoutManager(layoutManager);
+
+
+        //Week7
+        Uri uri = Uri.parse("content://fit2081.week8.movie.provider/movies");
+        Cursor result= getContentResolver().query(uri,null,null,null);
+        movies.setText(result.getCount()+"");
+    }
+}
